42deb34839c22b6b456c28c4f392fc0459b8367a
andypandypi <andrew.cullen@ucdconnect.ie>
2019-07-26 11:20:16 +0100

    Tangle AIMD scripts

---
+++ AIMD_main.m
@@ -0,0 +1,76 @@
+function AIMD_main(simTime, lambda, nNodes, orphanAge, orphanRate, alpha_AIMD, beta_AIMD)
+
+Tangle.nNodes = nNodes;
+Tangle.orphanAge = orphanAge;
+Tangle.orphanRate = orphanRate;
+Tangle.bAvgFilt = (1/10)*ones(10,1);
+
+Tangle.simTime = simTime;
+Tangle.q = 1/3;
+Tangle.dt = 0.1; % must be inverse of a whole number
+
+if(length(lambda)==1)
+    Tangle.lambda = lambda*ones(1, simTime/Tangle.dt); % rate of honest arrivals
+elseif(length(lambda)==simTime/Tangle.dt)
+    Tangle.lambda = lambda;
+else
+    error('lambda input is incorrect length');
+end
+
+Tangle.mu = 0;      % rate of malicious arrivals
+Tangle.nCW = 1;      % store this many past values of CW
+Tangle.h = 1;
+Tangle.maxDepth = 20;
+Tangle.nStartingTips = 10;
+Tangle.size = Tangle.nStartingTips;
+for k = 1:Tangle.nStartingTips
+    Tangle.Sites(k) = newsite(1, 1, Tangle.nCW, 0);
+    Tangle.Sites(k).isTip = 1;
+    Tangle.Sites(k).isAttached = 1;
+    Tangle.Sites(k).selectedParents = 1;
+    Tangle.Sites(k).timePending = Tangle.dt;
+end
+
+for n = 1:Tangle.nNodes
+    Tangle.Nodes(n).alpha = 0.1;
+    Tangle.Nodes(n).beta = 0;
+    Tangle.Nodes(n).alpha_AIMD = alpha_AIMD;
+    Tangle.Nodes(n).beta_AIMD = beta_AIMD;
+end
+
+Results.alphas = zeros(nNodes, simTime);
+Results.orphanRate = zeros(nNodes, simTime);
+Results.avgOrphanRate = zeros(nNodes, simTime);
+Results.nOrphans = zeros(nNodes, simTime);
+
+Results.nTips = zeros(1, simTime/Tangle.dt);
+
+[~, Results] = tangleAIMD(Tangle, 1, Results);
+
+%% Plotting of results
+% save('Results_basic.mat', 'Results'); 
+
+close all
+IEEEFigure
+
+figure(1)
+hold on
+c = get(groot,'DefaultAxesColorOrder');
+for i = 1:size(Results.alphas,1)
+    plot(1:size(Results.alphas,2), Results.alphas(i,:), 'Color', c(i,:));
+    plot([0 size(Results.alphas,2)], [mean(Results.alphas(i,:)) mean(Results.alphas(i,:))], 'Color', c(i,:), 'LineStyle', '--');
+end
+
+ylabel('$\alpha$','Interpreter', 'latex');
+xlabel('Time(s)');
+
+figure(2)
+hold on
+c = get(groot,'DefaultAxesColorOrder');
+for i = 1:size(Results.alphas,1)
+    plot(1:Tangle.simTime, Results.avgOrphanRate(i,:), 'Color', c(i,:));
+end
+
+ylabel('Orphan Rate (moving average)','Interpreter', 'latex');
+xlabel('Time(s)');
+
---
+++ tangleAIMD.m
@@ -0,0 +1,104 @@
+function [Tangle, Results] = tangleAIMD(Tangle, mcNum, Results)
+
+%% Tangle Simulation
+
+% Create waitbar
+f = waitbar(0, num2str(mcNum),'Name','Tangle Iteration Completion Bar',...
+    'CreateCancelBtn','setappdata(gcbf,''canceling'', 1)');
+
+setappdata(f,'canceling',0);
+
+for t = 0:Tangle.dt:Tangle.simTime-Tangle.dt
+    % Update waitbar
+    if getappdata(f,'canceling')
+        break
+    end
+    waitbar(t/Tangle.simTime,f)
+    
+    % AIMD update every second
+    if(floor(t)==t && t~=0)
+        nOrphans = 0;
+        for n = 1:Tangle.nNodes
+            Results.alphas(n,t) = Tangle.Nodes(n).alpha;
+            
+            nodeOrphans = find(([Tangle.Sites.node]==n).*[Tangle.Sites.isTip].*([Tangle.Sites.age]>=Tangle.orphanAge));
+            
+            for o = nodeOrphans
+                Tangle.Sites(o).node = 0; % remove association between this node and its orphaned site
+            end
+            
+            nOrphans = nOrphans + length(nodeOrphans);
+            if(t>1)
+                Results.orphanRate(n,t) = length(nodeOrphans);
+                Results.avgOrphanRate(n,:) = filter(Tangle.bAvgFilt,1,Results.orphanRate(n,:));
+                Results.nOrphans(n,t) = Results.nOrphans(n,t-1) + length(nodeOrphans);
+            else
+                Results.orphanRate(n,t) = length(nodeOrphans);
+                Results.avgOrphanRate(n,t) = length(nodeOrphans);
+                Results.nOrphans(n,t) = length(nodeOrphans);
+            end
+            
+            if(Results.orphanRate(n,t)<Tangle.orphanRate) % additive increase
+                Tangle.Nodes(n).alpha = Tangle.Nodes(n).alpha + Tangle.Nodes(n).alpha_AIMD;
+            else % multiplicative decrease
+                Tangle.Nodes(n).alpha = Tangle.Nodes(n).alpha*Tangle.Nodes(n).beta_AIMD;
+            end
+        end
+        
+%         if(nOrphans>0)
+%             backOffNode = ceil(Tangle.nNodes*rand());
+%         else
+%             backOffNode = 0;
+%         end
+%         
+%         for n = 1:Tangle.nNodes
+%             if(n~=backOffNode) % additive increase
+%                 Tangle.Nodes(n).alpha = Tangle.Nodes(n).alpha + Tangle.Nodes(n).alpha_AIMD;
+%             else % multiplicative decrease
+%                 Tangle.Nodes(n).alpha = Tangle.Nodes(n).alpha*Tangle.Nodes(n).beta_AIMD;
+%             end
+%         end
+        
+    end
+    
+    % add new arrivals for this time step by AIMD
+    for node = 1:Tangle.nNodes
+        if(t/Tangle.dt < Tangle.simTime)
+            Tangle = newarrivals(Tangle, Tangle.lambda(ceil((t+Tangle.dt)/Tangle.dt))/Tangle.nNodes, 0, 0, node); % each node has equal proportion of computing power
+        end
+    end
+    
+    for i = 1:Tangle.size
+        % set new CW as unchanged to begin with
+        Tangle.Sites(i).cumulativeWeight = [Tangle.Sites(i).cumulativeWeight(2:Tangle.nCW) Tangle.Sites(i).cumulativeWeight(Tangle.nCW)];
+        % if this site is not yet added to the tangle (pending)
+        if ~Tangle.Sites(i).isTip && ~Tangle.Sites(i).isSelected
+            
+            % if the site has found suitable tips to validate - PoW in
+            % progress
+            if Tangle.Sites(i).selectedParents
+                % do PoW
+                Tangle = doPoW(Tangle, i);
+                if Tangle.Sites(i).isAttached
+                    continue
+                end
+            % else look for some tips to validate
+            else
+                n = Tangle.Sites(i).node;
+                Tangle = selecttips(Tangle, i, Tangle.Nodes(n).alpha, Tangle.Nodes(n).beta, Tangle.Nodes(n).alpha, Tangle.Nodes(n).beta);
+                
+                Tangle.Sites(i).timePending = Tangle.Sites(i).timePending + Tangle.dt;
+            end
+            
+        % else, this has been added to the tangle so increment its age
+        else
+            Tangle.Sites(i).age = Tangle.Sites(i).age + Tangle.dt;
+            
+        end        
+    end
+    
+end
+Results.FinalTangles(mcNum) = Tangle;
+close(f);
+delete(f);
+
